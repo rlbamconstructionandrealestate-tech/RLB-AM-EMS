0e4ba6e4fdfbfd45740e602113a2d75f27a8cf35
rlbamconstructionandrealestate-tech <rlbamconstructionandrealestate@gmail.com>
2026-07-11 12:20:51 +0300

    Update settings.py for PostgreSQL on Render

--- config/settings.py
+++ config/settings.py
@@ -102,24 +102,16 @@ TEMPLATES = [
 # DATABASE
 # ======================================================
 
-if DEBUG:
-    DATABASES = {
-        "default": {
-            "ENGINE": "django.db.backends.sqlite3",
-            "NAME": BASE_DIR / "db.sqlite3",
-        }
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.getenv("DATABASE_NAME"),
+        "USER": os.getenv("DATABASE_USER"),
+        "PASSWORD": os.getenv("DATABASE_PASSWORD"),
+        "HOST": os.getenv("DATABASE_HOST"),
+        "PORT": os.getenv("DATABASE_PORT", "5432"),
     }
-else:
-    DATABASES = {
-        "default": {
-            "ENGINE": "django.db.backends.postgresql",
-            "NAME": os.getenv("DATABASE_NAME"),
-            "USER": os.getenv("DATABASE_USER"),
-            "PASSWORD": os.getenv("DATABASE_PASSWORD"),
-            "HOST": os.getenv("DATABASE_HOST"),
-            "PORT": os.getenv("DATABASE_PORT", "5432"),
-        }
-    }
+}
 
 
 # ======================================================
